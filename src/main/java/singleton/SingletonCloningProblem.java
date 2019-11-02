84251997cf0fd5d475d289d1fe940e6be93b5b66
tpinho <tiaho_pinho@optum.com>
2019-11-02 22:16:14 +0000

    Singleton package created

--- src/main/java/singleton/SingletonCloningProblem.java
+++ src/main/java/singleton/SingletonCloningProblem.java
@@ -1,4 +1,4 @@
-package main.java;
+package main.java.singleton;
 
 public class SingletonCloningProblem implements Cloneable {
 
--- src/main/java/singleton/SingletonSerializationSolution.java
+++ src/main/java/singleton/SingletonSerializationSolution.java
@@ -1,4 +1,4 @@
-package main.java;
+package main.java.singleton;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
